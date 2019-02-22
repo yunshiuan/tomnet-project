073c689d1590e67d5d3568af768cf3280eb480f6
yunshiuan <yunshiuan.chuang@gmail.com>
2019-02-22 16:53:48 +0800

    Replace test_model by test_model_performance

--- test_model_performance.py
+++ test_model_performance.py
@@ -1,9 +1,4 @@
-import sys
-sys.path.insert(0, '/Users/vimchiz/bitbucket_local/observer_model_group')
-import resnet as rn
-import data_handler as dh  
-import main_model as mm
-
-# Constants
-model = mm.Model()
+from main_model import Model
+test_model = Model()
 model.test()
+    
