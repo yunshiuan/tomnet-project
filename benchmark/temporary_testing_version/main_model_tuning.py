717b5b84afc4d9d227163da5f4d1b07367a33872
YunShiuan on Server <vimchiz@gmail.com>
2019-05-09 15:13:44 +0800

    Prepare to train: commit d42c3b

--- benchmark/temporary_testing_version/main_model_tuning.py
+++ benchmark/temporary_testing_version/main_model_tuning.py
@@ -268,8 +268,8 @@ class Model:
     print('Combination with best matches was ' + str(combinations[best]))
     print('Matches: ' + str(matches[best]) + '/' + str(length))
     print('Accuracy: ' + str(round(matches[best]*100/length,2)) + '%')
-    df = pd.DataFrame(data={'matches':str(matches[best]) + '/' + str(length)),
-                            'test_accurary':str(str(round(matches[best]*100/length,2)) + '%'},
+    df = pd.DataFrame(data={'matches':str(str(matches[best]) + '/' + str(length)),
+                            'test_accurary':str(str(round(matches[best]*100/length,2)) + '%')},
                       index = [0])
     # write the csv
     df.to_csv(self.train_path + '_test_accuracy.csv')
@@ -431,8 +431,8 @@ if __name__ == "__main__":
         EPOCH_SIZE = 80000
         DECAY_STEP_0 = 10000
         DECAY_STEP_1 = 15000
-        ckpt_fname = 'training_result/caches/cache_S002a_commit_31cb84_epoch80000_tuning_batch96_train_step_4000000_INIT_LR_0.00001' + str(times)
-        train_fname = 'training_result/caches/cache_S002a_commit_31cb84_epoch80000_tuning_batch96_train_step_4000000_INIT_LR_0.00001' + str(times)
+        ckpt_fname = 'training_result/caches/cache_S002a_v2_commit_d42c3b_epoch80000_tuning_batch96_train_step_10M_INIT_LR_10-5' + str(times)
+        train_fname = 'training_result/caches/cache_S002a_v2_commit_d42c3b_epoch80000_tuning_batch96_train_step_10M_INIT_LR_10-5' + str(times)
         sub_dir='/../S002a/'
 
         model = Model(args,BATCH_SIZE_TRAIN,BATCH_SIZE_VAL, BATCH_SIZE_TEST, TRAIN_STEPS, EPOCH_SIZE,DECAY_STEP_0, DECAY_STEP_1, ckpt_fname, train_fname, sub_dir)
