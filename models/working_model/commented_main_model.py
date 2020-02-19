edd8602ea44df70be2b4d6e16ca2a281a8e8b9b4
yunshiuan_PC <yunshiuan.chuang@gmail.com>
2020-02-19 00:28:40 -0600

    Prepare to train: 'human, v11', add S030

--- models/working_model/commented_main_model.py
+++ models/working_model/commented_main_model.py
@@ -1009,8 +1009,9 @@ if __name__ == "__main__":
   # Constants
   # --------------------------------------------------------
   # LIST_SUBJECTS = ["S0" + str(i) for i in ["35","50","51","52"]]
+  LIST_SUBJECTS = ["S030"]
 
-  LIST_SUBJECTS = ["S0" + str(i) for i in ["24","33","35","50","51","52"]]
+  # LIST_SUBJECTS = ["S0" + str(i) for i in ["24","33","35","50","51","52"]]
 
   # --------------------------------------------------------
   # Iterate through the subject list
@@ -1034,4 +1035,7 @@ if __name__ == "__main__":
       model.train()
     if args.mode == 'test' or args.mode == 'all':
       model.test()
+    print("------------------------------------")
+    print("Congratultions! You have reached the end of the script.")
+    print("------------------------------------")
 
