6354296d3d9d9fe82e106d78ad11a79be30671ff
yunshiuan_PC <yunshiuan.chuang@gmail.com>
2020-03-03 22:40:59 -0600

    Revise 'filter_full_targets_trajectories.R': skip subjects based on precise count of trajectories with full targets

--- scripts/filter_full_targets_trajectories/filter_full_targets_trajectories.R
+++ scripts/filter_full_targets_trajectories/filter_full_targets_trajectories.R
@@ -7,7 +7,21 @@
 library(stringr)
 # Constants-----------------------------------------------
 # Parameter
-LIST_SUBJ <- paste0("S0", c(24, 30, 33, 35, 50, 51, 52))
+LIST_SUBJ <- paste0(
+  "S0",
+  c(
+    24, 26, 28,
+    30, 31, 33,
+    35, 36, 37,
+    40, 43, 45,
+    46, 50, 51,
+    52, 53, 55,
+    58, 59, 60,
+    61, 62, 63,
+    65, 66, 67,
+    69
+  )
+)
 MAX_TARGETS <- 4
 
 # Path
@@ -15,13 +29,20 @@ PATH_ROOT <- str_extract(getwd(), pattern = ".*tomnet-project")
 PATH_DATA_ROOT <- file.path(PATH_ROOT, "data", "data_human")
 PATH_DATA_INPUT <- file.path(PATH_DATA_ROOT, "processed", LIST_SUBJ)
 PATH_TXT_OUTPUT <- file.path(PATH_DATA_ROOT, "filtered", LIST_SUBJ)
+
 # File
+# - for checking if the subject has been fully filtered
+FILE_COUNT_SUMMARY = file.path(PATH_DATA_ROOT,
+                               "processed",
+                               "summary_count_targets_2020-03-03.csv")
+df_count_summary = 
+  read.csv(FILE_COUNT_SUMMARY,header = T,stringsAsFactors = F)
 # Convert-------------------------------------------------
 for (subj_index in 1:length(PATH_DATA_INPUT)) {
   # local constants --------------------------------
   subj_path_data_input <- PATH_DATA_INPUT[subj_index]
   subj_txt_output <- PATH_TXT_OUTPUT[subj_index]
-  subj_name <- LIST_SUBJ[subj_index]
+  this_subj_name <- LIST_SUBJ[subj_index]
 
   if (!dir.exists(subj_txt_output)) {
     dir.create(subj_txt_output)
@@ -31,22 +52,25 @@ for (subj_index in 1:length(PATH_DATA_INPUT)) {
     path = subj_path_data_input, recursive = F, pattern = ".*txt"
   )
 
-  cat(paste0("Start processing ", subj_name, ".\n"))
+  cat(paste0("Start processing ", this_subj_name, ".\n"))
   # exception handling --------------------------------
   # check if there is any files in the dir
   if ((length(txt_raw_files) == 0)) {
-    warning(paste0(subj_name, " has no input files."))
+    warning(paste0(this_subj_name, " has no input files."))
     next
   }
   # check if the files have already been filtered
   txt_processed_files <- list.files(
     path = subj_txt_output, recursive = F, pattern = ".*txt"
   )
-
+  # - the number of trajectories with full targets
+  num_full_targets = 
+    subset(df_count_summary,
+           subj_name == this_subj_name & num_targets == MAX_TARGETS)$freq
   # skip if already filtered
-  if ((length(txt_raw_files) >= length(txt_processed_files)) & (length(txt_processed_files) > 0)) {
+  if ((length(txt_processed_files) == num_full_targets) & (length(txt_processed_files) > 0)) {
     warning(paste0(
-      subj_name,
+      this_subj_name,
       " has already been processed.", "\n",
       "#input files = ", length(txt_raw_files), "\n",
       "#filtered files = ", length(txt_processed_files), "\n"
